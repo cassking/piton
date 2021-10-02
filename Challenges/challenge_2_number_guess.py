fae4ad48b4d4b9d9a1dbb2172c7629df48496543
Cassandra King <puddincat@gmail.com>
2021-10-02 19:30:36 -0400

    update

--- Challenges/challenge_2_number_guess.py
+++ Challenges/challenge_2_number_guess.py
@@ -13,7 +13,7 @@ import random
 def run_game():
     answer: int = random.randint(1, 20)
     print("I'm thinking of a number between 1 and 20")
-    number_guesses = 3
+    number_guesses = 5
 
     while True:
         print(f'you have {number_guesses} left')
@@ -22,14 +22,12 @@ def run_game():
         if guess == answer:
             print(f"that is correct, game over the answer {answer}")
             return
+        elif number_guesses == 0:
+            print(f"game ended, the number was {answer}")
+            break
         elif guess < answer:
             print(f"that number is lower than the number")
-        elif number_guesses == 0:
-            break
         else:
             print(f"that number is higher than the number")
-        if number_guesses < 1:
-            print(f"game ended, the number was {answer}")
-
 
 run_game()
